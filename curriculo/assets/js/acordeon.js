ab1635a81361eaf0c43cae978f50513ac384253e
Filipe Rosa <filiperosa0312@gmail.com>
2025-09-11 23:39:49 -0300

    Atualizacao curriculo

--- curriculo/assets/js/acordeon.js
+++ curriculo/assets/js/acordeon.js
@@ -3,7 +3,7 @@ const acordeonTrigger = document.querySelectorAll('.acordeon .trigger')
 acordeonTrigger.forEach((trigger) => {
     trigger.addEventListener('click', (e) => {
         const acordeon = trigger.parentElement;
-        const isOpen = acordeon.classList.contains('open'); // Correção: Adiciona esta linha
+        const isOpen = acordeon.classList.contains('open'); 
 
         if (isOpen) {
             acordeon.classList.remove('open');
